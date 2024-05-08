0291ebbf18ba884918a017a7411138776c79bf05
ryannthegr8 <mmbaleryan@gmail.com>
2024-05-08 15:32:21 +0300

    working login

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -8,6 +8,31 @@ use Illuminate\Validation\Rule;
 
 class userController extends Controller
 {
+    public function showCorrectHomePage(){
+        if (auth()->check()) {
+            return view('homepage-feed');
+        } else {
+            return view('homepage');
+        }
+
+    }
+    public function login(Request $request) {
+        $incomingFields = $request -> validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        //Checks if the user has an account
+        if (Auth()->attempt(['username'=>$incomingFields['loginusername'], 'password'=>$incomingFields['loginpassword']])) {
+
+            //Logs the user in
+            $request->session()->regenerate();
+            return 'Logged in';
+        } else {
+            return 'Wrong username or password';
+        }
+
+    }
     public function register(Request $request){
         $incomingFields = $request->validate([
             'username' => ['required','min:3', 'max:20', Rule::unique('users', 'username')],
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\exampleController;
 use App\Http\Controllers\userController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [exampleController::class, "homePage"]);
+Route::get('/', [userController::class, "showCorrectHomePage"]);
 Route::get('/about', [exampleController::class, "aboutPage"]);
 
 Route::post('/register', [userController::class, "register"]);
+Route::post('/login', [userController::class, "login"]);
